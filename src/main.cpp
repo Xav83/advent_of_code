7c43e4af621a85aaa38258a308667419e6965201
Xav83 <x.jouvenot@gmail.com>
2022-12-04 23:31:46 +0100

    Allows to specify the solution for the examples depending on the puzzle

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,17 @@
 using Day = short;
 using Year = unsigned int;
 
-void printSolutionsOfDay(Day day, Year year, auto solver) {
+void printSolutionsOfDay(Day day, Year year, auto solver,
+                         PuzzleSolution expectedExamplePart1Solution,
+                         PuzzleSolution expectedExamplePart2Solution) {
   auto exampleLocation =
       fmt::format("../res/{}_day_{}_example_input.txt", year, day);
   auto exampleSolutionPart1 =
       solver.computeFirstPartSolution(getFileContext(exampleLocation));
   fmt::print("[{}-12-{}] Solution of the example for the part 1: {} [{}]\n",
              year, day, exampleSolutionPart1,
-             (exampleSolutionPart1 == 24000 ? "Success" : "FAIL"));
+             (exampleSolutionPart1 == expectedExamplePart1Solution ? "Success"
+                                                                   : "FAIL"));
 
   auto inputLocation = fmt::format("../res/{}_day_{}_input.txt", year, day);
   auto part1Solution =
@@ -23,7 +26,8 @@ void printSolutionsOfDay(Day day, Year year, auto solver) {
       solver.computeSecondPartSolution(getFileContext(exampleLocation));
   fmt::print("[{}-12-{}] Solution of the example for the part 2: {} [{}]\n",
              year, day, exampleSolutionPart2,
-             (exampleSolutionPart2 == 45000 ? "Success" : "FAIL"));
+             (exampleSolutionPart2 == expectedExamplePart2Solution ? "Success"
+                                                                   : "FAIL"));
 
   auto part2Solution =
       solver.computeSecondPartSolution(getFileContext(inputLocation));
@@ -33,7 +37,7 @@ void printSolutionsOfDay(Day day, Year year, auto solver) {
 
 int main() {
 
-  printSolutionsOfDay(01, 2022, aoc_2022::Day1Solver());
+  printSolutionsOfDay(01, 2022, aoc_2022::Day1Solver(), 24000, 45000);
 
   return 0;
 }
