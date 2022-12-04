687c2a607a36beac236c3a2966b001c8ef1ad4ee
Xav83 <x.jouvenot@gmail.com>
2022-12-04 22:48:20 +0100

    Adds the implementation needed to solve the example of the second part of day1 2022 puzzle.

--- src/2022/day_1_calorie_counting.cpp
+++ src/2022/day_1_calorie_counting.cpp
@@ -1,5 +1,7 @@
 #include "day_1_calorie_counting.hpp"
+#include <numeric>
 #include <string>
+#include <vector>
 
 using aoc_2022::Day1Solver;
 
@@ -23,11 +25,48 @@ Day1Solver::computeFirstPartSolution(const std::string_view input) {
     caloriesCarriedByCurrentElf += std::stoi(std::string(it, endOfNumber));
     it = std::next(endOfNumber);
   }
+  // last elf
+  maxCaloriesCarryByOneElf =
+      std::max(maxCaloriesCarryByOneElf, caloriesCarriedByCurrentElf);
 
   return maxCaloriesCarryByOneElf;
 }
 
+#include <iostream>
+
 PuzzleSolution
 Day1Solver::computeSecondPartSolution(const std::string_view input) {
-  return 0;
+  auto it = input.begin();
+  std::vector<Calories> maxCaloriesCarryByTopThreeElves = {0, 0, 0};
+  Calories caloriesCarriedByCurrentElf = 0;
+
+  while (it != input.end()) {
+    if (*it == '\n') {
+
+      maxCaloriesCarryByTopThreeElves.emplace_back(caloriesCarriedByCurrentElf);
+      maxCaloriesCarryByTopThreeElves.erase(
+          std::min_element(maxCaloriesCarryByTopThreeElves.begin(),
+                           maxCaloriesCarryByTopThreeElves.end()));
+      caloriesCarriedByCurrentElf = 0;
+      ++it;
+      continue;
+    }
+    auto endOfNumber = std::find(it, input.end(), '\n');
+    caloriesCarriedByCurrentElf += std::stoi(std::string(it, endOfNumber));
+    it = std::next(endOfNumber);
+  }
+
+  // last elf
+  maxCaloriesCarryByTopThreeElves.emplace_back(caloriesCarriedByCurrentElf);
+  maxCaloriesCarryByTopThreeElves.erase(
+      std::min_element(maxCaloriesCarryByTopThreeElves.begin(),
+                       maxCaloriesCarryByTopThreeElves.end()));
+
+  std::cout << "Top " << maxCaloriesCarryByTopThreeElves.size() << ": "
+            << maxCaloriesCarryByTopThreeElves[0] << ", "
+            << maxCaloriesCarryByTopThreeElves[1] << ", "
+            << maxCaloriesCarryByTopThreeElves[2] << std::endl;
+
+  return std::accumulate(maxCaloriesCarryByTopThreeElves.begin(),
+                         maxCaloriesCarryByTopThreeElves.end(), 0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,23 @@
 int main() {
 
   aoc_2022::Day1Solver day1Solver;
-  auto exampleSolution = day1Solver.computeFirstPartSolution(
+  auto exampleSolutionPart1 = day1Solver.computeFirstPartSolution(
       getFileContext("../res/2022_day_1_example_input.txt"));
-  std::cout << "[2022-12-01] Solution of the example: " << exampleSolution
-            << (exampleSolution == 24000 ? " [Success]" : "FAIL") << std::endl;
+  std::cout << "[2022-12-01] Solution of the example for the part 1: "
+            << exampleSolutionPart1
+            << (exampleSolutionPart1 == 24000 ? " [Success]" : " [FAIL]")
+            << std::endl;
 
   auto part1Solution = day1Solver.computeFirstPartSolution(
       getFileContext("../res/2022_day_1_part_1_input.txt"));
   std::cout << "[2022-12-01] Solution of the part 1: " << part1Solution
+            << std::endl;
+
+  auto exampleSolutionPart2 = day1Solver.computeSecondPartSolution(
+      getFileContext("../res/2022_day_1_example_input.txt"));
+  std::cout << "[2022-12-01] Solution of the example for the part 2: "
+            << exampleSolutionPart2
+            << (exampleSolutionPart2 == 45000 ? " [Success]" : " [FAIL]")
             << std::endl;
 
   return 0;
