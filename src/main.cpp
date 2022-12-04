ac49efd776e10cbb9ee88af88e55ef804623ff57
Xav83 <x.jouvenot@gmail.com>
2022-12-04 23:01:08 +0100

    Factorizes the code of the main function in order to easily add the solution for the other days.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
 #include "2022/day_1_calorie_counting.hpp"
 #include <iostream>
 
-int main() {
+using Day = short;
+using Year = unsigned int;
 
-  aoc_2022::Day1Solver day1Solver;
-  auto exampleSolutionPart1 = day1Solver.computeFirstPartSolution(
-      getFileContext("../res/2022_day_1_example_input.txt"));
-  std::cout << "[2022-12-01] Solution of the example for the part 1: "
+void printSolutionsOfDay(Day day, Year year, auto solver) {
+  auto exampleSolutionPart1 = solver.computeFirstPartSolution(
+      getFileContext("../res/" + std::to_string(year) + "_day_" +
+                     std::to_string(day) + "_example_input.txt"));
+  std::cout << "[" << year << "-12-" << day
+            << "] Solution of the example for the part 1: "
             << exampleSolutionPart1
             << (exampleSolutionPart1 == 24000 ? " [Success]" : " [FAIL]")
             << std::endl;
 
-  auto part1Solution = day1Solver.computeFirstPartSolution(
-      getFileContext("../res/2022_day_1_input.txt"));
-  std::cout << "[2022-12-01] Solution of the part 1: " << part1Solution
-            << std::endl;
+  auto part1Solution = solver.computeFirstPartSolution(
+      getFileContext("../res/" + std::to_string(year) + "_day_" +
+                     std::to_string(day) + "_input.txt"));
+  std::cout << "[" << year << "-12-" << day
+            << "] Solution of the part 1: " << part1Solution << std::endl;
 
-  auto exampleSolutionPart2 = day1Solver.computeSecondPartSolution(
-      getFileContext("../res/2022_day_1_example_input.txt"));
-  std::cout << "[2022-12-01] Solution of the example for the part 2: "
+  auto exampleSolutionPart2 = solver.computeSecondPartSolution(
+      getFileContext("../res/" + std::to_string(year) + "_day_" +
+                     std::to_string(day) + "_example_input.txt"));
+  std::cout << "[" << year << "-12-" << day
+            << "] Solution of the example for the part 2: "
             << exampleSolutionPart2
             << (exampleSolutionPart2 == 45000 ? " [Success]" : " [FAIL]")
             << std::endl;
 
-  auto part2Solution = day1Solver.computeSecondPartSolution(
-      getFileContext("../res/2022_day_1_input.txt"));
-  std::cout << "[2022-12-01] Solution of the part 2: " << part2Solution
-            << std::endl;
+  auto part2Solution = solver.computeSecondPartSolution(
+      getFileContext("../res/" + std::to_string(year) + "_day_" +
+                     std::to_string(day) + "_input.txt"));
+  std::cout << "[" << year << "-12-" << day
+            << "] Solution of the part 2: " << part2Solution << std::endl;
+}
+
+int main() {
+
+  printSolutionsOfDay(01, 2022, aoc_2022::Day1Solver());
 
   return 0;
 }
